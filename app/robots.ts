2d10e6222e8a3d1b1e3f059ffaec1ba7d0c77c2b
ObjetivoCesar <objetivo.cesar@gmail.com>
2026-05-19 08:20:21 -0500

    fix(seo): allow bots to crawl dynamic og-images

--- app/robots.ts
+++ app/robots.ts
@@ -4,9 +4,10 @@ export default function robots(): MetadataRoute.Robots {
     return {
         rules: {
             userAgent: '*',
-            allow: '/',
+            allow: ['/', '/api/og-image/'],
             disallow: ['/admin/', '/api/'],
         },
         sitemap: 'https://www.activaqr.com/sitemap.xml',
     }
 }
+
